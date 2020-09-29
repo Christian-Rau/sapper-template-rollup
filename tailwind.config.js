bdb1d71ea1a4a186870c6e53544167d0858ec405
rauchristian <rauchristian@hotmail.com>
2020-09-29 14:11:21 +0200

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 module.exports = {
-<<<<<<< HEAD
 	future: {
 		removeDeprecatedGapUtilities: true,
 		purgeLayersByDefault: true,
 	},
 	purge: false,
-=======
-	purge: true,
->>>>>>> 62cfcbc1b45724de6c4bed28024eb60bdfd80e52
 	theme: {
 		extend: {},
 	},
