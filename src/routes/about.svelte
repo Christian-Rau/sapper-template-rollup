b4c9f712b7cfc341668819ac075364805eb8c70e
Christian Rau <rauchristian@hotmail.com>
2020-10-06 17:40:47 +0200

    .

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,29 @@
+<script>
+
+</script>
+
+<style>
+	.custom-btn {
+		@apply bg-blue-500 px-4 py-3 shadow rounded;
+	}
+</style>
+
 <svelte:head>
 	<title>About</title>
 </svelte:head>
 
 <h1>About this site</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p>This is the 'about' page.</p>
+
+<div class="text-3xl italic text-purple-800">
+	I’m a purple-colored italic text
+</div>
+
+<div>
+	<div>
+		<button class="custom-btn">
+			I’m a red button. Click me
+		</button>
+	</div>
+</div>
